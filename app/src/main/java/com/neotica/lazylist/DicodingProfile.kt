d2ba815f61572c77909645fa534d3440a226ad27
Ryo Martin <laetuzg@gmail.com>
2023-05-09 23:53:06 +0700

    Fixed margin and cleaning codes.

--- app/src/main/java/com/neotica/lazylist/DicodingProfile.kt
+++ app/src/main/java/com/neotica/lazylist/DicodingProfile.kt
@@ -3,60 +3,71 @@ package com.neotica.lazylist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.TopCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.neotica.dicoding2.R
-import com.neotica.lazylist.data.model.PersonEntity
 import com.neotica.dicoding2.ui.theme.Dicoding2Theme
 
 class DicodingProfile : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
+            rememberScrollState()
             Dicoding2Theme {
                 // A surface container using the 'background' color from the theme
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Topbar2()
-                    BoxWithConstraints(
-                        Modifier
-                            .height(685.dp)
-                            .align(CenterHorizontally)
-                            .background(androidx.compose.ui.graphics.Color.Black)) {
-
-                            Column(modifier = Modifier
-                                .fillMaxSize()
-                                .verticalScroll(ScrollState(1)),
-                            horizontalAlignment = CenterHorizontally
-                                ) {
+                Scaffold() {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        TopBarProfile()
+                        BoxWithConstraints(
+                            Modifier
+                                .height(685.dp)
+                                .align(CenterHorizontally)
+                                .background(androidx.compose.ui.graphics.Color.Black)
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(ScrollState(1)),
+                                horizontalAlignment = CenterHorizontally
+                            ) {
                                 Box(
                                     Modifier
                                         .height(690.dp)
-                                ){
+                                ) {
                                     Column(horizontalAlignment = CenterHorizontally) {
-                                        Box(contentAlignment = TopCenter){ ProfPic() }
+                                        Box(contentAlignment = TopCenter) { ProfPic() }
 
                                         Box(modifier = Modifier.padding(20.dp)) {
                                             Text(
-                                                modifier = Modifier.height(10.dp).padding(10.dp).aspectRatio(5.5f),
+                                                modifier = Modifier
+                                                    .height(10.dp)
+                                                    .padding(10.dp)
+                                                    .aspectRatio(5.5f),
                                                 text = "Nama: ",
                                                 color = androidx.compose.ui.graphics.Color.White,
                                                 fontSize = 18.sp,
@@ -68,10 +79,12 @@ class DicodingProfile : ComponentActivity() {
                                                 fontSize = 30.sp,
                                             )
                                         }
-
                                         Box(modifier = Modifier.padding(20.dp)) {
                                             Text(
-                                                modifier = Modifier.height(10.dp).padding(10.dp).aspectRatio(5.5f),
+                                                modifier = Modifier
+                                                    .height(10.dp)
+                                                    .padding(10.dp)
+                                                    .aspectRatio(5.5f),
                                                 text = "Email: ",
                                                 color = androidx.compose.ui.graphics.Color.White,
                                                 fontSize = 18.sp,
@@ -84,19 +97,15 @@ class DicodingProfile : ComponentActivity() {
                                             )
                                         }
                                     }
-
-
-                }
-
-                    }
-
+                                }
+                            }
                         }
                     }
                 }
-                //SAMPE SINI
             }
         }
     }
+}
 
 
 @Composable
@@ -108,91 +117,6 @@ private fun ProfPic() {
         modifier = Modifier
             .padding(20.dp)
             .size(200.dp)
-            .clip(RoundedCornerShape(corner = CornerSize(100.dp))),
-        //alignment = Alignment.TopCenter,
-
+            .clip(RoundedCornerShape(corner = CornerSize(100.dp)))
     )
-}
-
-@Composable
-private fun ProfileHeader(
-    scrollState: ScrollState,
-    puppy: PersonEntity,
-    containerHeight: Dp
-) {
-    val offset = (scrollState.value / 2)
-    val offsetDp = with(LocalDensity.current) { offset.toDp() }
-
-    Image(
-        modifier = Modifier
-            .heightIn(max = containerHeight / 2)
-            .fillMaxWidth()
-            .padding(top = offsetDp),
-        painter = painterResource(id = puppy.personImageId),
-        contentScale = ContentScale.Crop,
-        contentDescription = null
-    )
-}
-
-@Composable
-private fun ProfileContent(puppy: PersonEntity, containerHeight: Dp) {
-    Column {
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Name(puppy)
-
-        ProfileProperty2(stringResource(R.string.sex), puppy.sex)
-
-        ProfileProperty2(stringResource(R.string.age), puppy.age.toString())
-
-        ProfileProperty2(stringResource(R.string.personality), puppy.description)
-
-        // Add a spacer that always shows part (320.dp) of the fields list regardless of the device,
-        // in order to always leave some content at the top.
-        Spacer(Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
-    }
-}
-
-@Composable
-private fun Name(
-) {
-    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
-
-            Text(text = "One")
-
-    }
-}
-
-@Composable
-private fun Name(puppy: PersonEntity, modifier: Modifier = Modifier) {
-    Text(
-        text = puppy.title,
-        modifier = modifier,
-        style = MaterialTheme.typography.h5,
-        fontWeight = FontWeight.Bold
-    )
-}
-
-@Composable
-fun ProfileProperty2(label: String, value: String, isLink: Boolean = false) {
-    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
-        Divider()
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(
-                text = label,
-                modifier = Modifier.baselineHeight(24.dp),
-                style = MaterialTheme.typography.caption,
-            )
-        }
-        val style = if (isLink) {
-            MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.primary)
-        } else {
-            MaterialTheme.typography.body1
-        }
-        Text(
-            text = value,
-            modifier = Modifier.baselineHeight(24.dp),
-            style = style
-        )
-    }
 }
--- app/src/main/java/com/neotica/lazylist/ProfileScreen.kt
+++ app/src/main/java/com/neotica/lazylist/ProfileScreen.kt
@@ -1,29 +1,18 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.neotica.lazylist
 
+import android.app.Activity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -36,41 +25,64 @@ import com.neotica.lazylist.data.DataProvider
 import com.neotica.lazylist.data.model.PersonEntity
 
 @Composable
-fun ProfileScreen(person: PersonEntity, onNavIconPressed: () -> Unit = { }) {
+fun ProfileScreen(person: PersonEntity) {
     val scrollState = rememberScrollState()
 
-    Column(modifier = Modifier.fillMaxSize().background(color = Color.Black)) {
-        BoxWithConstraints(modifier = Modifier.weight(1f)) {
-            Topbar2()
-            Box(Modifier.height(685.dp).align(Alignment.BottomCenter)){
-                Surface {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .verticalScroll(scrollState),
-                    ) {
-                        ProfileHeader(
-                            scrollState,
-                            person,
-                            this@BoxWithConstraints.maxHeight
-                        )
-                        ProfileContent(person, this@BoxWithConstraints.maxHeight)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+    ) {
+        Column {
+            TopBarProfile()
+            BoxWithConstraints(modifier = Modifier.weight(1f)) {
+                Box {
+                    Surface {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .verticalScroll(scrollState),
+                        ) {
+                            ProfileHeader(
+                                scrollState,
+                                person,
+                                this@BoxWithConstraints.maxHeight
+                            )
+                            ProfileContent(person, this@BoxWithConstraints.maxHeight)
+                        }
                     }
                 }
             }
-
-            /*AdoptFab(
-                extended = scrollState.value == 0,
-                modifier = Modifier.align(Alignment.BottomEnd)
-            )*/
         }
+    }
+}
+
+@Composable
+fun TopBarProfile() {
+    Column {
+        TopAppBar(
+            title = {
+                Text(text = "Profile")
+            },
+            navigationIcon = {
+                val context = LocalContext.current as? Activity
+                Image(
+                    painter = painterResource(R.drawable.back), contentDescription = null,
+                    modifier = Modifier
+                        .clickable { context?.finish() }
+                        .padding(8.dp)
+                        .size(84.dp)
+                        .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+                )
+            }
+        )
     }
 }
 
 @Composable
 private fun ProfileHeader(
     scrollState: ScrollState,
-    puppy: PersonEntity,
+    entity: PersonEntity,
     containerHeight: Dp
 ) {
     val offset = (scrollState.value / 2)
@@ -81,47 +93,45 @@ private fun ProfileHeader(
             .heightIn(max = containerHeight / 2)
             .fillMaxWidth()
             .padding(top = offsetDp),
-        painter = painterResource(id = puppy.personImageId),
+        painter = painterResource(id = entity.personImageId),
         contentScale = ContentScale.Crop,
         contentDescription = null
     )
 }
 
 @Composable
-private fun ProfileContent(puppy: PersonEntity, containerHeight: Dp) {
+private fun ProfileContent(entity: PersonEntity, containerHeight: Dp) {
     Column {
         Spacer(modifier = Modifier.height(8.dp))
 
-        Name(puppy)
+        Name(entity)
 
-        ProfileProperty(stringResource(R.string.sex), puppy.sex)
+        ProfileProperty(stringResource(R.string.sex), entity.sex)
 
-        ProfileProperty(stringResource(R.string.age), puppy.age.toString())
+        ProfileProperty(stringResource(R.string.age), entity.age.toString())
 
-        ProfileProperty(stringResource(R.string.personality), puppy.description)
+        ProfileProperty(stringResource(R.string.personality), entity.description)
 
-        // Add a spacer that always shows part (320.dp) of the fields list regardless of the device,
-        // in order to always leave some content at the top.
         Spacer(Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
     }
 }
 
 @Composable
 private fun Name(
-    puppy: PersonEntity
+    entity: PersonEntity
 ) {
     Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
         Name(
-            puppy = puppy,
+            entity = entity,
             modifier = Modifier.baselineHeight(32.dp)
         )
     }
 }
 
 @Composable
-private fun Name(puppy: PersonEntity, modifier: Modifier = Modifier) {
+private fun Name(entity: PersonEntity, modifier: Modifier = Modifier) {
     Text(
-        text = puppy.title,
+        text = entity.title,
         modifier = modifier,
         style = MaterialTheme.typography.h5,
         fontWeight = FontWeight.Bold
@@ -151,40 +161,10 @@ fun ProfileProperty(label: String, value: String, isLink: Boolean = false) {
         )
     }
 }
-/*
-@Composable
-fun AdoptFab(extended: Boolean, modifier: Modifier = Modifier) {
-    FloatingActionButton(
-        onClick = { *//* TODO *//* },
-        modifier = modifier
-            .padding(16.dp)
-            .padding()
-            .height(48.dp)
-            .widthIn(min = 48.dp),
-        backgroundColor = Purple500,
-        contentColor = Color.White
-    ) {
-        AnimatingFabContent(
-            icon = {
-                Icon(
-                    imageVector = Icons.Outlined.Call,
-                    contentDescription = stringResource(R.string.adopt_me)
-                )
-            },
-            text = {
-                Text(
-                    text = stringResource(R.string.adopt_me),
-                )
-            },
-            extended = extended
-
-        )
-    }
-}*/
 
 @Preview
 @Composable
 fun ProfilePreview() {
-    val puppy = DataProvider.person
-    ProfileScreen(person = puppy)
+    val artist = DataProvider.person
+    ProfileScreen(person = artist)
 }
